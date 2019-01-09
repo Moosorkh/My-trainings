cb27b5d964beda2c1deb3093a40144e6157a0db1
Mehdi Azar <34349323+Moosorkh@users.noreply.github.com>
2019-01-09 12:40:08 -0800

    Fast Linear Search updatet

---
+++ CodeRecipes/CodeRecipes/FastLinearSearch.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FastLinearSearch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Search(List<int> list, int x)
+            {
+                var i = 0;
+                var counts = list.Count;
+                list.Add(x);
+                while (true)
+                {
+                    if (list[i] == x)
+                        return i < counts ? i : -1;
+                    i++;
+                }
+            }
+            var items = new List<int> { 2, 3, 5, 7, 11, 13, 17 };
+
+            Console.WriteLine(Search(items, 1));
+            //print -1
+            Console.WriteLine(Search(items, 7));
+            //print 3
+            Console.WriteLine(Search(items, 19));
+            //print -1
+
+            //*** simplified speed test ***
+
+            items = (new int[10000000]).Select((var, i) => i).ToList();
+            var count = 100;
+
+            var start = DateTime.Now;
+
+            for (int i = 0; i < count; i++)
+            {
+                Search(items, 7777777);
+            }
+            var delta = DateTime.Now - start;
+            var milliseconds = delta.TotalMilliseconds / count;
+
+            Console.WriteLine(milliseconds);
+        }
+    }
+}
