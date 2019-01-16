05d37d236cacc2cb37fe6611f46160462791c232
Mehdi <moosorkh@gmail.com>
2019-01-15 23:17:20 -0800

    The new Code recipe files added

---
+++ CodeRecipes/Thread Class/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading;
+
+namespace Thread_Class
+{
+
+    public class MXThread
+    {
+        //Non-Static method
+        public void mythr()
+        {
+            for (int J = 0; J < 2; J++)
+            {
+                Console.WriteLine("My thread is" + " in progress...!!");
+            }
+        }
+    }
+   public class GFG
+    {
+        static void Main()
+        {
+            //Creating object of ExThread class
+            MXThread obj = new MXThread();
+            //Creating and initializing thread
+            //Using thread class and 
+            //ThreadStart constructor
+            Thread t = new Thread(new ThreadStart(obj.mythr));
+            t.Start();
+        }
+    }
+}
