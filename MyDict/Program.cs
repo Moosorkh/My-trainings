3408104a025f724ffaac9563bc1593f484bf872a
Mehdi Azar <34349323+Moosorkh@users.noreply.github.com>
2019-01-18 18:19:50 -0800

    Some fies were deleted

---
+++ MyDict/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyDict
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Create a new SortedDictionary
+            //of strings, with string keys
+            SortedDictionary<string, string> myDict = new SortedDictionary<string, string>();
+
+            //Adding key/value pairs in myDict
+            myDict.Add("Australia", "Canberra");
+            myDict.Add("Belgium", "Brussels");
+            myDict.Add("NetherLand", "Amsterdam");
+            myDict.Add("China", "Beijing");
+            myDict.Add("Russia", "Moscow");
+            myDict.Add("India", "Delhi");
+            // To get count of key/value pairs in myDict
+
+            Console.WriteLine("Total key/value pairs"+" in myDict are: "+ myDict.Count);
+
+            //To get the keys alone,
+            //use the keys property
+            SortedDictionary<string, string>.KeyCollection keyColl = myDict.Keys;
+
+            //The elements of the MyCollection
+            //Are strongly typed with the type
+            //that was specified for dictionary keys
+            foreach (string s in keyColl)
+            {
+                Console.WriteLine($"Key = {s}");
+            }
+        }
+    }
+}
